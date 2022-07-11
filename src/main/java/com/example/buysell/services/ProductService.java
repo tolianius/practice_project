3cd6bf4879560b03dc3181900f900abd0f40e8f5
DESKTOP-M53DAJU\tolia <tolianius23@gmail.com>
2022-07-11 14:36:53 +0300

    test_application-1

---
+++ src/main/java/com/example/buysell/services/ProductService.java
@@ -0,0 +1,36 @@
+package com.example.buysell.services;
+
+import com.example.buysell.models.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+    private List<Product> products = new ArrayList<>();
+    private long ID = 0;
+
+    {
+        products.add(new Product(++ID, "PlayStation 5", "Simple description", 67000, "Krasnodar ", "baringo"));
+        products.add(new Product(++ID, "Rtx 3050", "Simple description", 34000, "Moscow", "jake"));
+    }
+
+    public List<Product> listProducts() { return products; }
+
+    public void saveProduct(Product product) {
+        product.setId(++ID);
+        products.add(product);
+    }
+
+    public void deleteProduct(Long id) {
+        products.removeIf(product -> product.getId().equals(id));
+    }
+
+    public Product getProductById(Long id) {
+        for (Product product : products) {
+            if (product.getId().equals(id)) return product;
+        }
+        return null;
+    }
+}
